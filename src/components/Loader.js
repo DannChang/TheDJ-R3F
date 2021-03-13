d0b5ae003fdca15a750801a93ab4de800398ebfc
Dan Chang <dmagma@hotmail.com>
2021-03-13 09:51:40 -0800

    Minor fixes

---
+++ src/components/Loader.js
@@ -0,0 +1,25 @@
+import { useProgress } from 'drei'
+
+//React Spring
+import { a, useTransition } from '@react-spring/web'
+
+function Loader() {
+    const { active, progress } = useProgress();
+    const transition = useTransition(active, {
+      from: { opacity: 1, progress: 0 },
+      leave: { opacity: 0 },
+      update: { progress },
+    });
+    return transition(
+      ({ progress, opacity }, active) =>
+        active && (
+          <a.div className='loading' style={{ opacity }}>
+            <div className='loading-bar-container'>
+              <a.div className='loading-bar' style={{ width: progress }}></a.div>
+            </div>
+          </a.div>
+        )
+    );
+  }
+
+export default Loader;
--- src/components/Model.js
+++ src/components/Model.js
@@ -2,7 +2,6 @@ import { useGLTFLoader } from 'drei';
 
 const Model = ( { modelPath }) => {
     const gltf = useGLTFLoader(modelPath, true)
-    console.log(gltf)
 
     return (
       <primitive object={gltf.scene} dispose={null} />
